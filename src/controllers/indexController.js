4de3ed2f9d030dc76ae2a986f2d0daaf00e2a7dc
marianotorrecilla <marianotorrecilla89@hotmail.com>
2020-12-03 15:15:28 -0300

    calse 3 taller mercado pago

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,8 +1,112 @@
+const mercadopago = require('mercadopago')
+
+mercadopago.configure({
+    access_token : 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
+    integrator_id : 'dev_24c65fb163bf11ea96500242ac130004',
+})
+
 module.exports = {
     home: (req, res) => {
         return res.render("index");
     },
     detail: (req, res) => {
         return res.render("detail", { ...req.query });
-    }
+    },
+
+    callback: (req, res) => {
+        console.log(req.query);
+        
+        if(req.query.status.includes('success')) {
+            return res.render('success')
+        }
+
+        if(req.query.status.includes('pending')) {
+            return res.render('pending')
+        }
+
+        if(req.query.status.includes('failure')) {
+            return res.render('failure')
+        }
+        
+        return res.status(404).end()
+    },
+
+    notifications : (req, res) => {
+
+        console.log(req.body);
+
+        res.status(200).end('Ok')
+    },
+
+    comprar: (req, res) => {
+
+        const host = 'http://mi-app.herokuapp.com/'
+
+        const url = host + 'callback?status='
+
+        let preference = {
+
+            back_urls : {
+                success : url + 'success',
+                pending: url + 'pending',
+                failure : url + 'failure',
+            },
+
+            notification_url : host + 'notifications',
+
+            auto_return : 'approved',
+
+            payer : {
+                name : 'Ryan',
+                surname: 'Dahl',
+                email: 'test_user_63274575@testuser.com',
+                phone: {
+                    area_code: '11',
+                    number: 55556666,
+                },
+                address: {
+                    street_name: 'Monroe',
+                    zip_code: '1234',
+                    street_number: 860
+                }
+            },
+
+            payment_methods : {
+
+                excluded_payment_methods : [
+                    {id : 'visa'}
+                ],
+
+                excluded_payment_types : [
+                    {id : 'atm'}
+                ],
+
+                installments : 12
+            },
+
+            items : [
+                {
+                    id : '1234',
+                    picture_url : 'https://mercadopago2020.herokuapp.com/images/products/jordan.jpg',
+                    title : 'Nombre del producto',
+                    description : 'Dispositivo móvil de Tienda e-commerce',
+                    unit_price : 999,
+                    quantity: 1  
+                }  
+            ],
+            external_reference: 'marianotorrecilla89@hotmail.com',
+        }
+
+        mercadopago.preferences.create(preference).then(response => {
+
+            global.init_point = response.body.init_point
+            res.render('confirm')
+
+
+        }).catch(error => {
+            console.log(error);
+            res.send('error')
+        })
+
+    },
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,4 +9,15 @@ router.get('/', indexController.home);
 /* GET detail page */
 router.get('/detail', indexController.detail);
 
+/*redireccion back urls */
+
+router.get('/callback', indexController.callback);
+
+/* notificaciones -----  no esta localmente para testear solo con el deployed*/
+router.post('/notifications', indexController.notifications);
+
+
+/* POST compra */
+router.post('/comprar', indexController.comprar);
+
 module.exports = router;
